175170922a8576a96a414f23441f1d2abac6d59e
Luiz Henrique Baldão <luizhbfilho@outlook.com>
2022-09-19 10:54:18 -0300

    Optmized methods

--- app/src/main/java/br/com/alura/orgs/ui/dialog/FormularioImagemDialog.kt
+++ app/src/main/java/br/com/alura/orgs/ui/dialog/FormularioImagemDialog.kt
@@ -3,29 +3,41 @@ package br.com.alura.orgs.ui.dialog
 import android.content.Context
 import android.util.Log
 import android.view.LayoutInflater
-import android.widget.ImageView
 import androidx.appcompat.app.AlertDialog
 import br.com.alura.orgs.databinding.FormularioImagemBinding
 import br.com.alura.orgs.extensions.tentaCarregarImagem
 
 class FormularioImagemDialog(private val context: Context) {
-    fun mostra(quandoImagemCarregada: (imagem: String) -> Unit){
-        val binding = FormularioImagemBinding
-            .inflate(LayoutInflater.from(context))
-        binding.formularioImagemBotaoCarregar.setOnClickListener {
-            val url = binding.formularioImagemUrl.text.toString()
-            binding.formularioImagemImageview.tentaCarregarImagem(url)
-        }
-        AlertDialog.Builder(context)
-            .setView(binding.root)
-            .setPositiveButton("Confirmar") { _, _ ->
-                val url = binding.formularioImagemUrl.text.toString()
-                Log.i("FormularioImagem", "mostra: $url")
-                quandoImagemCarregada(url)
-            }
-            .setNegativeButton("Cancelar") { _, _ ->
+    fun mostra(
+        urlPadrao: String? = null,
+        quandoImagemCarregada: (imagem: String) -> Unit
+    )   {
+        FormularioImagemBinding
+            .inflate(LayoutInflater.from(context)).apply {
 
+                urlPadrao?.let {
+                    formularioImagemImageview.tentaCarregarImagem(it)
+                    formularioImagemUrl.setText(it)
+                }
+
+                formularioImagemBotaoCarregar.setOnClickListener {
+                    val url = formularioImagemUrl.text.toString()
+                    formularioImagemImageview.tentaCarregarImagem(url)
+                }
+                AlertDialog.Builder(context)
+                    .setView(root)
+                    .setPositiveButton("Confirmar") { _, _ ->
+                        val url = formularioImagemUrl.text.toString()
+                        quandoImagemCarregada(url)
+                    }
+                    .setNegativeButton("Cancelar") { _, _ ->
+
+                    }
+                    .show()
             }
-            .show()
+
+
+
+
     }
 }
