e6883eb566c1bed9be15d94badbaba2f9bdbd057
luizhbfilho <luizhbfilho@outlook.com>
2022-10-19 12:18:40 -0300

    New version

---
+++ app/src/main/java/br/com/alura/orgs/ui/activity/DetalhesProdutoActivity.kt
@@ -0,0 +1,57 @@
+package br.com.alura.orgs.ui.activity
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import br.com.alura.orgs.R
+import br.com.alura.orgs.databinding.ActivityDetalhesProdutoBinding
+import br.com.alura.orgs.extensions.formataParaMoedaBr
+import br.com.alura.orgs.extensions.tentaCarregarImagem
+import br.com.alura.orgs.model.Produto
+
+class DetalhesProdutoActivity: AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityDetalhesProdutoBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        tentaCarregarProduto()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_detalhes_produto, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_detalhes_produto_remover -> {
+
+            }
+            R.id.menu_detalhes_produto_editar -> {
+
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun tentaCarregarProduto() {
+        intent.getParcelableExtra<Produto>(CHAVE_PRODUTO)?.let { produtoCarregado ->
+            preencheCampos(produtoCarregado)
+        } ?: finish()
+    }
+
+    private fun preencheCampos(produtoCarregado: Produto) {
+        with(binding) {
+            activityDetalhesProdutoImagem.tentaCarregarImagem(produtoCarregado.imagem)
+            activityDetalhesProdutoNome.text = produtoCarregado.nome
+            activityDetalhesProdutoDescricao.text = produtoCarregado.descricao
+            activityDetalhesProdutoValor.text =
+                produtoCarregado.valor.formataParaMoedaBr()
+        }
+    }
+}
